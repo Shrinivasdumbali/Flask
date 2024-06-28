16d0b8ddd747d5fcecd7cb5f373278772fc874fa
Shrinivas C Dumbali <106445572+Shrinivasdumbali@users.noreply.github.com>
2024-06-28 17:19:53 +0000

    modified

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def shrini():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
