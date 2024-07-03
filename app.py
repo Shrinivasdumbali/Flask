f7a492372c66b168a68ffd3fe3b29f42e88f674c
Shrinivas C Dumbali <106445572+Shrinivasdumbali@users.noreply.github.com>
2024-07-03 14:58:50 +0000

    changes!!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify  #(html is jsonifyed!!)
 
-app = Flask(__name__)
+app = Flask(
+    __name__)  #flask variable "app" is used further while deploying over cloud
 
 JOBS = [{
     'id': 1,
